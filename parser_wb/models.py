e6273b929ae6febb2542f9f55dc98c4169c2dd27
Alexander_Baranov <Sti_ll@mail.ru>
2025-06-29 18:33:27 +0300

    migrate model

--- parser_wb/models.py
+++ parser_wb/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название товара')
+    price = models.PositiveIntegerField(verbose_name='цена')
+    price_discount = models.PositiveIntegerField(verbose_name='цена со скидкой')
+    rating = models.DecimalField(max_digits=10, decimal_places=1, verbose_name='рейтинг')
+    feedbacks = models.PositiveIntegerField(verbose_name='количество отзывов')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
