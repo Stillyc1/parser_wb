c0bfb7575b57c87654445defa29b58a91852fb35
Alexander_Baranov <Sti_ll@mail.ru>
2025-06-29 18:34:21 +0300

    add serializers.py

--- parser_wb/urls.py
+++ parser_wb/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
+
 from parser_wb.apps import ParserWbConfig
+from parser_wb.views import ProductListAPIView, ProductListView
 
 app_name = ParserWbConfig.name
 
 urlpatterns = [
-
+    path('', ProductListView.as_view(), name='products-list'),
+    path('api/products/', ProductListAPIView.as_view(), name='products-api-list'),
 ]
